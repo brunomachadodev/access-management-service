a3b3bb6fe13add8aefcd0e34342c8a6aeb10771d
Bruno Machado <bruno-mla@hotmail.com>
2025-08-27 15:38:32 -0300

    chore: update dependencies and add Swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { OpenAPIObject } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+import * as swaggerUi from 'swagger-ui-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  try {
+    const swaggerYaml: string = fs.readFileSync('./swagger.yaml', 'utf8');
+    const swaggerDocument = yaml.load(swaggerYaml) as OpenAPIObject;
+
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  } catch (e) {
+    console.warn('Could not load swagger.yaml', e);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
