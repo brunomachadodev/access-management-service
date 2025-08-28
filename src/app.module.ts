5a100fbce23af1403ed55457e8b028183485ce35
Bruno Machado <bruno-mla@hotmail.com>
2025-08-28 11:52:39 -0300

    refactor: create individual module for database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 import { ScopesModule } from './modules/scopes/scopes.module';
 import { UserScopesModule } from './modules/user-scopes/user-scopes.module';
 
@@ -11,20 +11,7 @@ import { UserScopesModule } from './modules/user-scopes/user-scopes.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [__dirname + '/database/entities/*.entity{.ts,.js}'],
-        synchronize: false,
-      }),
-    }),
+    DatabaseModule,
     ScopesModule,
     UserScopesModule,
     AuthModule,
